bb1995fcd7107202557169db10030a1fe37d46e5
Enrique-Ruiz-ECU <80851860+Enrique-Ruiz-ECU@users.noreply.github.com>
2021-05-10 04:58:19 +0200

    Add files via upload

---
+++ empresa.sql
@@ -0,0 +1,15 @@
+create schema empresa;
+use empresa;
+CREATE TABLE `usuarios` (
+  Id int(11) NOT NULL AUTO_INCREMENT,
+  Nombre varchar(20) DEFAULT NULL,
+  Apellido varchar(20) DEFAULT NULL,
+  Usuario varchar(20) DEFAULT NULL,
+  Contrasena varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+);
+create user 'usuario' identified by '123';
+GRANT USAGE ON empresa.* TO 'usuario'@'%';
+grant all privileges on empresa.* to 'usuario'@'%';
+
+select * from usuarios;
